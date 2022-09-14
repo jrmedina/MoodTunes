3290e655624d5cba77338ddef465fe20755fe94d
jmedina <ndgns2@gmail.com>
2022-09-14 09:16:49 -0600

    Add/ fav icon and adjusted header

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,11 +4,12 @@ import Skull from '../../assets/skull.png'
 
 const NavBar = () => {
   return (
-    <div className='nav-container'>
-      <h1 className='title'>MoodTunes</h1>
-      <img className='skull' src={Skull}/>
+    <div className="nav-container">
+      <h1 className="title">Mood</h1>
+      <img className="skull" src={Skull} />
+      <h1 className="title">Tunes</h1>
     </div>
-  )
+  );
 }
 
 export default NavBar
