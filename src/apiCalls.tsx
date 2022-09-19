a1e512b026a897159759f4464ff1b9fc39edc71f
jmedina <ndgns2@gmail.com>
2022-09-19 16:07:16 -0600

    Initial deploy

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,6 +1,6 @@
 const fetchData = async () => {
   try {
-    const response = await fetch("http://localhost:3001");
+    const response = await fetch("https://moodtunes-api.herokuapp.com/");
     const json = await response.json();
     return json;
   } catch (error) {
