b6139bc99d731c9c08269fbf388efd596f7f631f
jmedina <ndgns2@gmail.com>
2022-12-05 14:21:56 -0700

    redeploy link

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,6 +1,6 @@
 const fetchData = async () => {
   try {
-    const response = await fetch("https://moodtunes-api.herokuapp.com/");
+    const response = await fetch("https://moodtunes-api.onrender.com/");
     const json = await response.json();
     return json;
   } catch (error) {
