2d0d68bda2e2f6ab9c00fcb831977e43695c855e
jmedina <ndgns2@gmail.com>
2022-12-06 09:05:59 -0700

    redeploy

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,6 +1,6 @@
 const fetchData = async () => {
   try {
-    const response = await fetch("https://moodtunes-api.onrender.com/");
+    const response = await fetch("https://mood-tunes-api.vercel.app/");
     const json = await response.json();
     return json;
   } catch (error) {
